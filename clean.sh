af46b96bd11159a70e40c48f7022b83d726c902f
andrefabbro <andre.fabbro@liferay.com>
2024-02-23 18:51:05 -0300

    Fix ulimit and mysql version

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf bundles/data/document_library/*
+rm -rf bundles/data/elasticsearch-data/*
+rm -rf bundles/logs/*
+rm -rf bundles/deploy/*
+rm -rf bundles/osgi/configs/*
+rm -rf bundles/osgi/modules/*
+rm -rf bundles/osgi/war/*
